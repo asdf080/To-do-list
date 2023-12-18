0f77dc361ff2860a08937d03eca5a6f30a82bff6
Dahee Lee <ai04120@naver.com>
2023-12-18 12:41:05 +0900

    asdf

---
+++ quotes.js
@@ -0,0 +1,25 @@
+// 명언
+const quotes = document.querySelector('#quotes')
+
+fetch('https://api.adviceslip.com/advice')
+.then(res => res.json())
+.then(data => {
+    const word = data.slip.advice;
+    quotes.innerText = `"${word}"`
+  })
+
+// 날씨
+const weather = document.querySelector('#weather');
+function onSu(position){
+  const lat = position.coords.latitude
+  const lon = position.coords.longitude
+  const API = 'cf7b2cf5d9459d0374323134c46a2819'
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API}&units=metric`)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+    weather.innerText = data.weather[0].main + " " + data.main.temp + " ºC"
+  })
+}
+
+navigator.geolocation.getCurrentPosition(onSu, () => weather.innerText ="오류");
--- toDoList.js
+++ toDoList.js
@@ -52,3 +52,24 @@ if(saved){
   toDos=JSON.parse(saved)
   toDos.forEach(paintToDo);
 }
+
+// 이미지
+const randImg = document.querySelector("#randImg")
+const imgArr = ["1.jpg","2.jpg","3.jpg"]
+const ran = imgArr[Math.floor(Math.random()*imgArr.length)]
+
+const img = document.createElement("img")
+img.setAttribute("src",`images/${ran}`)
+randImg.appendChild(img)
+
+// 시계
+let clock = document.querySelector("#clock")
+function getClock(){
+  const date = new Date();
+  let ho = date.getHours();
+  let mi = date.getMinutes();
+  let se = date.getSeconds();
+  clock.innerHTML = `<i class="fa-regular fa-clock"></i>   ${String(ho).padStart(2,'0')} : ${String(mi).padStart(2,'0')} : ${String(se).padStart(2,'0')}`
+}
+getClock()
+setInterval(getClock, 1000)
